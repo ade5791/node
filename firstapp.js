6aae49d04cb11c3a9341545dafe5b7a5343fec92
ade5791 <adetola@live.com>
2020-06-21 11:42:06 -0400

    change app name

---
+++ firstapp.js
@@ -0,0 +1,15 @@
+const express = require ('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+ res.send("welcome to the home page baby");
+});
+
+const port = process.env.port || 3000;
+
+app.listen(port, () => {
+    console.log ("wazzappp");
+});
+
+
